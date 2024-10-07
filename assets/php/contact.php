3ceb9e6b7bed79699155f737ff2924fbf4541083
Laxman879 <laxman.a@brewcode.co>
2024-10-07 19:30:45 +0530

    new theme

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,5 +1,5 @@
 <?php
- 
+    // Only process POST requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
@@ -14,11 +14,14 @@
             exit;
         }
 
-     
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
         $recipient = "info@yourmail.com";
 
+        // Set the email subject.
         $subject = "New contact from $name";
 
+        // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n";
         
@@ -45,17 +48,22 @@
             $email_content .= "Message:\n$message\n";
         }
 
+        // Build the email headers.
         $email_headers = "From: $name <$email>";
 
+        // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
+            // Set a 500 (internal server error) response code.
             http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
+        // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
